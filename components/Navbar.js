0e4a577bc1fabb861c32c4a52df5e66739d6a12f
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-03-15 23:36:21 -0300

    Changing <a> tag to <Link>

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,20 @@
 import styles from '@/styles/Navbar.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function NavBar() {
     return (
         <> 
         <ul className={styles.navbar}>
             <div className={styles.select_itens_bar}>
-            <a><Image src="/lol_icon.png" alt="Lol Logo" width={35} height={35} priority /></a>
-            <a> Campeões</a>
-            <a> Itens</a>
-            <a> Runas </a>
+                <Link href={'/campeoes'}><Image src="/lol_icon.png" alt="Lol Logo" width={35} height={35} priority /></Link> 
+                <Link href={'/'}> Campeões</Link>
+                <Link href={'/'}> Itens</Link>
+                <Link href={'/'}> Runas </Link>
             </div>
             <div>
-            <a className={styles.button_login}> Entrar</a>
-            <a className={styles.button_signup}> Cadastra-se</a>
+                <a className={styles.button_login}> Entrar</a>
+                <a className={styles.button_signup}> Cadastra-se</a>
             </div>
       </ul>
       <div className={styles.divider_navbar}> </div>
