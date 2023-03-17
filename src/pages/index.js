f2ebf03507315c0c1a3981b91bca119bf53ca125
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-03-17 17:10:59 -0300

    Removing div and html card
    Removing html from card and inserting Carhome container, and removing unnecessary div

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,25 @@
 import Image from "next/image";
 import styles from "@/styles/Home.module.css"
+import CardHome from "../../components/CardHome";
 
 export default function Home() {
   return (
     <>
-    <section className={styles.home_page_section_image}>
-      <div className={styles.home_page_image_container}>
-        <Image className={styles.home_page_image} src='/images/home_page.png' width={960} height={544.5}  alt='home_page_image'/>
-        <Image className={styles.home_page_logo} src='/images/leagueoflegends.png' height={207.7} width={320}/>
-
-      </div>
+    <section className={styles.home_page_image_container}>
+      <Image className={styles.home_page_image} src='/images/home_page.png' width={960} height={544.5}  alt='home_page_image'/>
+      <Image className={styles.home_page_logo} src='/images/leagueoflegends.png' height={207.7} width={320} alt='League of legends'/>
     </section>
 
+
     <section className={styles.home_section_conteudo}>
-    <div className={styles.text_about}>
-        <p> Campeões (ou "Champions", em inglês) no League of Legends são personagens jogáveis que os jogadores podem escolher para participar das partidas. Cada campeão tem suas próprias habilidades e características, incluindo atributos como força, resistência, velocidade e habilidades especiais.
-        </p>
-        <p> Uma enciclopédia que busca trazer 
-        informações sobre campeões, builds e runas e 
-        lore de cada campeão de runeterra.
-        </p>
-        <p> Uma enciclopédia que busca trazer 
-        informações sobre campeões, builds e runas e 
-        lore de cada campeão de runeterra.
-        </p>
-    </div>
+      <CardHome imageUrl="/images/champs.png" title={"Campeões"} content='Os campeões de League of Legends (LoL) são personagens jogáveis do jogo. 
+        Cada campeão possui habilidades únicas, um papel específico dentro de uma equipe e um estilo de jogo próprio. Atualmente, existem mais de 150 campeões 
+        disponíveis no jogo.'/>
+      <CardHome imageUrl="/images/runaFeit.png" title={"Runas"} content='As runas em League of Legends são uma das principais formas de personalizar o seu campeão, 
+      permitindo que os jogadores escolham habilidades e bônus que se adequem ao seu estilo de jogo e às necessidades da partida.'/>
+      <CardHome imageUrl="/images/itens.png" title={"Itens"} content='Os itens em League of Legends (LoL) são equipamentos que podem ser comprados pelos jogadores 
+        durante uma partida para melhorar as habilidades do seu campeão. Existem diversos tipos de itens em LoL, cada um com uma função específica e que pode ser comprado 
+        com diferentes combinações de ouro.'/>
     </section>
     </>
   )
