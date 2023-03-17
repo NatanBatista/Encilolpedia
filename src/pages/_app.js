7dfb4309c2c904890ae0963e41802f9ca545719e
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-03-17 17:07:56 -0300

    Adding fonts to header app page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,15 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 import MainContainer from '../../components/MainContainer'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet"/>
+    </Head>
     <main>
         <MainContainer>
           <Component {...pageProps}/>
