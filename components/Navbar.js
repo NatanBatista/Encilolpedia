e09e0f28d2eea13657de0dce7c111041236b6d46
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-03-17 17:04:35 -0300

    Adding routes in navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,11 +6,11 @@ export default function NavBar() {
     return (
         <> 
         <ul className={styles.navbar}>
-            <div className={styles.select_itens_bar}>
-                <Link href={'/campeoes'}><Image src="/lol_icon.png" alt="Lol Logo" width={35} height={35} priority /></Link> 
-                <Link href={'/'}> Campeões</Link>
-                <Link href={'/'}> Itens</Link>
-                <Link href={'/'}> Runas </Link>
+            <div >
+                <Link href={'/'}><Image src="/lol_icon.png" alt="Lol Logo" width={35} height={35} priority /></Link> 
+                <Link className={styles.select_itens_bar} href={'/campeoes'}> Campeões</Link>
+                <Link className={styles.select_itens_bar} href={'/itens'}> Itens</Link>
+                <Link className={styles.select_itens_bar}href={'/runas'}> Runas </Link>
             </div>
             <div>
                 <a className={styles.button_login}> Entrar</a>
