665dfa9be7dea316fcab7251a56047489be9fcf8
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-03-19 23:17:09 -0300

    Adding validations and importing React

--- components/CardHome.js
+++ components/CardHome.js
@@ -1,29 +1,39 @@
-import styles from '@/styles/CardHome.module.css'
+import styles from "@/styles/CardHome.module.css";
+import React from "react";
+import PropTypes from "prop-types";
+
+
+CardHome.propTypes = {
+	imageUrl: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+	content: PropTypes.string.isRequired,
+};
+  
 
 export default function CardHome({ imageUrl, title, content }) {
-    return (
-        <>
-            <div className={styles.card_body}>
-                <div className={styles.card_body_background} style={{ backgroundImage: `url(${imageUrl})` }}> 
+	return (
+		<>
+			<div className={styles.card_body}>
+				<div className={styles.card_body_background} style={{ backgroundImage: `url(${imageUrl})` }}> 
                     
-                    <div className={styles.card_mov}>
+					<div className={styles.card_mov}>
 
-                        <div className={styles.push_card}>
+						<div className={styles.push_card}>
 
-                            <i class='material-icons'>expand_less</i>
+							<i className='material-icons'>expand_less</i>
 
-                        </div>
+						</div>
 
-                        <div className={styles.card_title}>
+						<div className={styles.card_title}>
                             
-                            {title}
+							{title}
 
-                        </div>
+						</div>
                         
-                        <p className={styles.card_content}> { content } </p>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+						<p className={styles.card_content}> { content } </p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
--- components/MainContainer.js
+++ components/MainContainer.js
@@ -1,12 +1,21 @@
-import Footer from './Footer';
-import NavBar from './Navbar';
+import React from "react";
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+
+import PropTypes from "prop-types";
+
+
+MainContainer.propTypes = {
+	children: PropTypes.node.isRequired,
+};
+
 
 export default function MainContainer(props) {
-  return (
-    <>
-      <NavBar/>
-      <div> {props.children} </div>
-      <Footer/>
-    </>
-  )
+	return (
+		<>
+			<NavBar/>
+			<div> {props.children} </div>
+			<Footer/>
+		</>
+	);
 }
