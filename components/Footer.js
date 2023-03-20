abc3dd933c363ca6a8adc090ee0db99bd6e3af8a
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-03-19 23:17:43 -0300

    Importing React

--- components/Footer.js
+++ components/Footer.js
@@ -1,43 +1,44 @@
-import styles from '@/styles/Footer.module.css'
-import Link from 'next/link'
+import styles from "@/styles/Footer.module.css";
+import Link from "next/link";
+import React from "react";
 
 export default function Footer(){
-    return (
-        <>
-            <section className={styles.section_footer}>
-                <ul className={styles.main_content}>
-                    <div className={styles.content}>
-                        <span> Contato </span>
-                        <li>
-                            <Link href={'https://github.com/NatanBatista/encilolpedia'} target='_blank' rel='noopener noreferrer'> 
+	return (
+		<>
+			<section className={styles.section_footer}>
+				<ul className={styles.main_content}>
+					<div className={styles.content}>
+						<span> Contato </span>
+						<li>
+							<Link href={"https://github.com/NatanBatista/encilolpedia"} target='_blank' rel='noopener noreferrer'> 
                                 Github
-                            </Link>
-                        </li>
-                        <li> 
-                            <Link href={'https://www.google.com'} target='_blank' rel='noopener noreferrer'> 
+							</Link>
+						</li>
+						<li> 
+							<Link href={"https://www.google.com"} target='_blank' rel='noopener noreferrer'> 
                                 Email
-                            </Link>
-                        </li>
-                    </div>
+							</Link>
+						</li>
+					</div>
 
-                    <div className={styles.content}>
-                        <span > Tecnologias </span>
-                        <li>
-                            <Link href={'https://nextjs.org'} target='_blank' rel='noopener noreferrer'> 
+					<div className={styles.content}>
+						<span > Tecnologias </span>
+						<li>
+							<Link href={"https://nextjs.org"} target='_blank' rel='noopener noreferrer'> 
                                 Nextjs 13
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href={'https://rubyonrails.org'} target='_blank' rel='noopener noreferrer'> 
+							</Link>
+						</li>
+						<li>
+							<Link href={"https://rubyonrails.org"} target='_blank' rel='noopener noreferrer'> 
                                 Ruby on Rails
-                            </Link>
-                        </li>
-                    </div>
-                </ul>
-                <div className={styles.copyright}> 
-                    <span> Copyright © 2023 Encilolpédia. All Rights Reserved. </span> 
-                </div>
-            </section>
-        </>
-    )
+							</Link>
+						</li>
+					</div>
+				</ul>
+				<div className={styles.copyright}> 
+					<span> Copyright © 2023 Encilolpédia. All Rights Reserved. </span> 
+				</div>
+			</section>
+		</>
+	);
 }
--- src/pages/campeoes/index.js
+++ src/pages/campeoes/index.js
@@ -1,57 +1,58 @@
-import styles from '@/styles/Campeao.module.css'
+import styles from "@/styles/Campeao.module.css";
+import React from "react";
 
 export default function Campeaos() {
-    return (
-        <>
-            <section className={styles.section_up}>
-                <div className={styles.container_title}> 
-                    <div className={styles.title}>
-                        <span> ESCOLHA SEU </span>
-                        <strong> CAMPEÃO </strong>
-                    </div>
-                    <div className={styles.title_content}>
-                        <span> 
+	return (
+		<>
+			<section className={styles.section_up}>
+				<div className={styles.container_title}> 
+					<div className={styles.title}>
+						<span> ESCOLHA SEU </span>
+						<strong> CAMPEÃO </strong>
+					</div>
+					<div className={styles.title_content}>
+						<span> 
                             Com mais de 140 Campeões, você 
                             encontrará a combinação perfeita para seu estilo de jogo. 
                             Especialize-se em um estilo ou em todos. 
-                        </span>
-                    </div>
-                </div>
+						</span>
+					</div>
+				</div>
 
-                <div className={styles.searchBar}>
-                    <div className={styles.search_by_name}>
-                        <i class='material-icons'>search</i>
-                        <p>Buscas</p>
-                    </div>
-                    <div className={styles.search_by_class}>
-                        <ul>
+				<div className={styles.searchBar}>
+					<div className={styles.search_by_name}>
+						<i className='material-icons'>search</i>
+						<p>Buscas</p>
+					</div>
+					<div className={styles.search_by_class}>
+						<ul>
                             TODOS
-                        </ul>
-                        <ul>
+						</ul>
+						<ul>
                             ASSASSINO
-                        </ul>
-                        <ul>
+						</ul>
+						<ul>
                             LUTADORES
-                        </ul>
-                        <ul>
+						</ul>
+						<ul>
                             MAGO
-                        </ul>
-                        <ul>
+						</ul>
+						<ul>
                             ATIRADORES
-                        </ul>
-                        <ul>
+						</ul>
+						<ul>
                             SUPORTES
-                        </ul>
-                        <ul>
+						</ul>
+						<ul>
                             TANQUES
-                        </ul>
-                    </div>
-                    <div className={styles.search_by_rank} >
-                        <span>Dificuldade</span>
-                        <i class='material-icons'>expand_more</i>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+						</ul>
+					</div>
+					<div className={styles.search_by_rank} >
+						<span>Dificuldade</span>
+						<i className='material-icons'>expand_more</i>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 }
